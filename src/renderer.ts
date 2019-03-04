88751d511eef63ac58a529f30e527f1cdd7a50b7
Juha Lundan <juha.lundan@gmail.com>
2019-03-04 23:31:18 +0200

    fix(Aurelia): Fix aurelia boot
    Was missing bootstrap etc. Add those.

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,15 @@
+/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
+// we want font-awesome to load as soon as possible to show the fa-spinner
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import { bootstrap } from 'aurelia-bootstrapper';
 import { Aurelia } from 'aurelia-framework';
 import { App } from './app';
 import environment from './environment';
+import * as Bluebird from 'bluebird';
+
+Bluebird.config({ warnings: { wForgottenReturn: false } });
 
 bootstrap(async (aurelia: Aurelia) => {
   aurelia.use.standardConfiguration();

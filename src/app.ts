26e44376cecc7521adfa4e6de2a81896e38c36c1
Juha Lundan <juha.lundan@gmail.com>
2019-03-05 01:00:51 +0200

    fix(Nav): Fix minor positioning issue

--- src/app.ts
+++ src/app.ts
@@ -25,8 +25,10 @@ export class App {
                 moduleId: PLATFORM.moduleName('./modules/settings/settings'),
                 nav: true,
                 title: 'Settings',
-                settings: {icons: ["cog"]},
-                cssClass: "settings"
+                settings: {
+                    icons: ["cog"],
+                    cssClass: "settings"
+                }
             },
         ]);
 

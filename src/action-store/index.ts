1aef5a92d7b0afd720db31be2cbe122f180a6799
Juha Lundan <juha.lundan@gmail.com>
2019-03-11 01:24:26 +0200

    refactor(Actions): Finally give up and go with Aurelia's way.
    At least register all actions in central place so we can keep track of
    those on some level.

--- src/action-store/index.ts
+++ src/action-store/index.ts
@@ -2,6 +2,7 @@ import {pluck, distinctUntilChanged} from "rxjs/operators"
 import {autoinject} from "aurelia-dependency-injection";
 import {Store} from "aurelia-store";
 import {State} from "../state/state";
+import {Connection} from "../state/connections";
 
 @autoinject()
 export class ActionStore {
@@ -9,64 +10,33 @@ export class ActionStore {
 
     constructor(private _store: Store<State>) {
         this._isAutoRegistering = true;
+        this.registerAddConnectionAction();
     }
 
-    public register(clazz) {
-        const instance = new Creator<Action>(clazz).getNew();
-
-        if(!(instance instanceof Action)) {
-            throw `ActionStore: Cannot fire class ${clazz.name} which is not Action`;
-        }
-
-        console.log(`ActionStore: Registering class ${clazz.name}`);
-
-        this._store.registerAction(clazz.name, instance.reducer)
+    public registerAddConnectionAction(){
+        this._store.registerAction('AddConnectionAction', (state: State, payload: any) => {
+            const newConnections = JSON.parse(JSON.stringify(state.viewStates.dbNavigator.connections));
+            newConnections.push(payload);
+            state.viewStates.dbNavigator.connections = newConnections;
+            return state;
+        })
     }
 
-    public fire(clazz, payload: any): Promise<void> {
-        return new Promise((resolve, reject) => {
-            try{
-                if(this._isAutoRegistering === true && !this._store.isActionRegistered(clazz.name)) {
-                    this.register(clazz);
-                }
-                resolve();
-            }catch (e) {
-                reject(new Error(e));
-            }
-        }).then(() => {
-            return this._store.dispatch(clazz.name, payload);
-        });
+    public fireAddConnectionAction(connection: Connection){
+        return this._store.dispatch('AddConnectionAction', connection);
     }
-}
-
-export abstract class Action {
-    public get reducer() {
-        return (state: State, payload: any): State => {
-            const newState = JSON.parse(JSON.stringify(state));
-            return this.perform(newState, payload);
-        };
-    }
-
-    protected abstract perform(state: State, any): State;
 
 }
 
-interface ParameterlessConstructor<T> {
-    new(): T;
-}
-
-class Creator<T> {
-    constructor(private ctor: ParameterlessConstructor<T>) {}
-    getNew() {
-        return new this.ctor();
-    }
-}
-
-export function filter(subState) {
-    const subStates = subState.split('.');
-    return {
-        selector: (store) => store.state.pipe(pluck(...subStates), distinctUntilChanged()),
+export function filter(filters) {
+    const result = {
         target: 'state',
-        onChanged: 'changeHandler'
+        selector: {}
+    };
+
+    for(const key of Object.keys(filters)) {
+        result.selector[key] = (store) => store.state.pipe(pluck(...(filters[key].split('.'))), distinctUntilChanged())
     }
+
+    return result;
 }

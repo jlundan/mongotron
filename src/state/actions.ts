9b6f186837f108485400af375005bea5d7cc47c1
Juha Lundan <juha.lundan@gmail.com>
2019-03-10 23:46:38 +0200

    Revert "refactor(Actions): Refactor ActionStore"
    This reverts commit 1a63ca6b
    
    Nope, no can do. This would lock the payload and reducer in resulting in using same payload for all requests. I am starting to give up on this.

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -3,11 +3,8 @@ import {Connection} from "./connections";
 import {Action} from "../action-store";
 
 export class AddConnectionAction extends Action {
-    constructor(private connection: Connection){
-        super ("ACTION_ADD_CONNECTION", connection);
-    }
-
-    public updateState(state: State, connection: Connection) {
+    protected perform(state: State, connection: Connection): State {
         state.viewStates.dbNavigator.connections.push(connection);
+        return state;
     }
 }
